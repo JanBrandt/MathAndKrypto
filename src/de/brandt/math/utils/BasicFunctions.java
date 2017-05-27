2151c4acc403bb127356354ccf726078d1613839
Jan Brandt <jbrandt1@freenet.de>
2017-05-27 17:03:51 +0200

    Function gcd added

--- src/de/brandt/math/utils/BasicFunctions.java
+++ src/de/brandt/math/utils/BasicFunctions.java
@@ -1,10 +1,37 @@
 
 package de.brandt.math.utils;
 
+/**
+ * @author Jan Brandt
+ */
 public final class BasicFunctions {
 
+  /**
+   * Computes the number of integers that are prime to the given number n.
+   *
+   * @param n A number > 0
+   * @return The number of integers that are prime to n
+   */
   public int eulerPhiFunction(final int n) {
-    return n;
+    int number = 0;
+    for (int i = 0; i < n; i++) {
+      if (gcd(n, i) == 1) {
+        number++;
+      }
+    }
+    return number;
+  }
+
+  /**
+   * Computes the greatest common divider of a and b.
+   *
+   * @param a First number
+   * @param b Second number
+   * @return The gcd
+   */
+  private int gcd(final int a, final int b) {
+    // TODO Auto-generated method stub
+    return 0;
   }
 
 }
