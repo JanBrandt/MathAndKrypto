595745baa79c94e669d2b60e4f7f5d10b350c97b
Jan Brandt <jbrandt1@freenet.de>
2017-05-27 17:02:26 +0200

    Firsts tests for TDD

---
+++ tests/de/brandt/math/utils/TestEulerPhiFunction.java
@@ -0,0 +1,19 @@
+
+package de.brandt.math.utils;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ * All tests of the Euler phi-function.
+ * @author Jan Brandt
+ *
+ */
+public class TestEulerPhiFunction {
+
+  @Test
+  public void testEulerPhiFunction() {
+    fail("Not yet implemented");
+  }
+
+}
