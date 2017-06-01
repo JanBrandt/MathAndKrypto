056e91505d7c1fe4a9e5bd4112c6c3a63f6b7ffe
Jan Brandt <jbrandt1@freenet.de>
2017-06-01 21:43:11 +0200

    Added testSuite and package-info for UtilsTest

---
+++ tests/de/brandt/math/utils/test/AllUtilsTests.java
@@ -0,0 +1,11 @@
+package de.brandt.math.utils.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestEulerPhiFunction.class, Testgcd.class })
+public class AllUtilsTests {
+
+}
---
+++ tests/de/brandt/math/utils/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Jan Brandt
+ * A collection of useful functions.
+ */
+package de.brandt.math.utils.test;
