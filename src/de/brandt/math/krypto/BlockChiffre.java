f307570954c8ba4bc100eb2832c73b141db14234
Jan Brandt <jbrandt1@freenet.de>
2017-06-02 15:14:14 +0200

    Made class abstract, first methods added

--- src/de/brandt/math/krypto/BlockChiffre.java
+++ src/de/brandt/math/krypto/BlockChiffre.java
@@ -8,5 +8,20 @@ package de.brandt.math.krypto;
  *
  */
 public abstract class BlockChiffre {
+  private int blockLength;
+  private EncryptionFunction encryptor;
 
+  /**
+   * @param length The length of each block of the chiffre.
+   */
+  public BlockChiffre(final int length) {
+    this.blockLength = length;
+  }
+
+  /**
+   * @return The blocklength of the chiffre.
+   */
+  public final int getBlockLength() {
+    return blockLength;
+  }
 }
