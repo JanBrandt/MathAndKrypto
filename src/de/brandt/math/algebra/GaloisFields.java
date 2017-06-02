3ca88b8cef8574d95f31c6b42944fd661cfcf575
Jan Brandt <jbrandt1@freenet.de>
2017-06-02 15:12:25 +0200

    first lines of code, constructor checks parameters

--- src/de/brandt/math/algebra/GaloisFields.java
+++ src/de/brandt/math/algebra/GaloisFields.java
@@ -1,10 +1,27 @@
 
 package de.brandt.math.algebra;
 
+import de.brandt.math.utils.Utils;
+
 /**
  * @author Jan Brandt
  *
  */
 public class GaloisFields {
+
+  /**
+   * Constructs a new Galoisfield GF(p^n).
+   * Checks for valid parameters first.
+   * @param p A prime number
+   * @param n A positive integer
+   */
+  public GaloisFields(final int p, final int n) {
+    if (p < 1 || n < 1) {
+      throw new IllegalArgumentException("Parameters are not positive");
+    }
+    if (!Utils.isPrime(p)) {
+      throw new IllegalArgumentException("p is not prime");
+    }
+  }
 
 }
