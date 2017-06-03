3580d6f71a96d7207d859eece17582d8cf57560f
Jan Brandt <jbrandt1@freenet.de>
2017-06-03 22:41:20 +0200

    Added javadoc

--- src/de/brandt/math/krypto/EncryptionFunction.java
+++ src/de/brandt/math/krypto/EncryptionFunction.java
@@ -1,6 +1,15 @@
 package de.brandt.math.krypto;
 
+/**
+ * @author Jan Brandt
+ *
+ */
 public abstract class EncryptionFunction {
 
-  public abstract String encrypt(final String message, final String key);
+  /**
+   * @param message The String that should be encrypted
+   * @param key The key, the message should be encrypted with
+   * @return The encrypted message
+   */
+  public abstract String encrypt(String message, String key);
 }
