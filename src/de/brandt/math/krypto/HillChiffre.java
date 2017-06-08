a1d0d4d8a507f94d4817c07c7298ace275905560
Jan Brandt <jbrandt1@freenet.de>
2017-06-08 20:41:52 +0200

    Added class HillChiffre, just stub

---
+++ src/de/brandt/math/krypto/HillChiffre.java
@@ -0,0 +1,22 @@
+package de.brandt.math.krypto;
+
+/**
+ * This class contains a implementation of the Hill-Chiffre, a universal linear
+ * Block-Chiffre. Lester S. Hill invented this chiffre in 1929.
+ * @author Jan Brandt
+ *
+ */
+public final class HillChiffre extends BlockChiffre {
+
+  public HillChiffre(final int length) {
+    super(length);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public String encrypt(final String message, final String key) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
