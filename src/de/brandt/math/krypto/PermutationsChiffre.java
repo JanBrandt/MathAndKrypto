4a62eeaaefc95ddcc69732edf4a85bd246422b50
Jan Brandt <jbrandt1@freenet.de>
2017-06-08 20:42:34 +0200

    Added class Permutations-Chiffre, just stub

---
+++ src/de/brandt/math/krypto/PermutationsChiffre.java
@@ -0,0 +1,21 @@
+package de.brandt.math.krypto;
+
+/**
+ * This class implements the classic permutation-chiffre.
+ * @author Jan Brandt
+ *
+ */
+public final class PermutationsChiffre extends BlockChiffre {
+
+  public PermutationsChiffre(final int length) {
+    super(length);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public String encrypt(final String message, final String key) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
