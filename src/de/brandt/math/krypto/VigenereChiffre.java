04390f7077fd327ab41020f71bdb8521c408ee39
Jan Brandt <jbrandt1@freenet.de>
2017-06-08 20:42:08 +0200

    Added class Vigenere-Chiffre, just stub

---
+++ src/de/brandt/math/krypto/VigenereChiffre.java
@@ -0,0 +1,23 @@
+package de.brandt.math.krypto;
+
+/**
+ * This class contains an implementation of the classic Vigenere-Chiffre, first
+ * described by Giovan Battista Bellaso in 1553.
+ * It is a polyalphabetic subscription and an affin linear BlockChiffre.
+ * @author Jan Brandt
+ *
+ */
+public final class VigenereChiffre extends BlockChiffre {
+
+  public VigenereChiffre(final int length) {
+    super(length);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public String encrypt(final String message, final String key) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
