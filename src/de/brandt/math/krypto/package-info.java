b412a2db39ba5a5b29ca2b46a8a4d4f1b37eefad
Jan Brandt <jbrandt1@freenet.de>
2017-06-08 20:43:49 +0200

    Upgraded package-info, added new classic crypto-functions

--- src/de/brandt/math/krypto/package-info.java
+++ src/de/brandt/math/krypto/package-info.java
@@ -1,5 +1,6 @@
 /**
  * @author Jan Brandt
  * A collection of functions for cryptography.
+ * Beginning with some historic cryptographic examples to modern Block-Chiffres.
  */
 package de.brandt.math.krypto;
